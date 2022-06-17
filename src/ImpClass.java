8230f7c2443e24ccf653266540d822cc6cce1fd7
ellafrankcom <ellafrankcom@gmail.com>
2022-06-17 13:01:28 +0100

    serializable

--- src/ImpClass.java
+++ src/ImpClass.java
@@ -1,5 +1,48 @@
+import java.io.*;
+import java.time.LocalDateTime;
+import java.util.*;
+
 public class ImpClass implements RemoteInt {
     public void serializable(){
-        System.out.println("Edit method for serializable");
+        Employee e = new Employee();
+        Scanner scanner = new Scanner(System.in);
+        LocalDateTime time = LocalDateTime.now();
+
+        System.out.println("Type your name: ");
+        e.name = scanner.nextLine();
+
+        System.out.println("Type the product you need: ");
+        e.product = scanner.nextLine();
+
+        System.out.println("Type the name of the store you need them sent to: ");
+        e.store = scanner.nextLine();
+
+        e.SSN = 12345678;
+        e.number = 101;
+
+
+        try{
+            FileOutputStream file = new FileOutputStream("Serialize.ser");
+            ObjectOutputStream out = new ObjectOutputStream(file);
+            out.writeObject(e);
+            out.writeObject(time);
+            out.close();
+            file.close();
+            System.out.println("Serialized data was saved in Serialize.ser");
+        }catch(IOException i){
+            i.printStackTrace();
+        }
+    }
+
+    public class Employee implements java.io.Serializable{
+        public String name;
+        public String product;
+        public String store;
+        public transient int SSN;
+        public int number;
+
+        public void mailProduct(){
+            System.out.println("Sending " + product + " to " + store + "store");
+        }
     }
 }
